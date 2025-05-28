ba8ed9b904b3edaf9977155244d64a90640d3297
Md. Asif ullah <mdasifullah456@gmail.com>
2025-05-29 00:43:26 +0600

    feat: add user profile functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const router = express.Router()
-const { registerUser, loginUser } = require('../controllers/authController')
+const express = require("express");
+const router = express.Router();
+const {
+  registerUser,
+  loginUser,
+  profile,
+} = require("../controllers/authController");
 
 // Register new user
-router.post('/register', registerUser)
+router.post("/register", registerUser);
 
 // Login user
-router.post('/login', loginUser)
+router.post("/login", loginUser);
 
-module.exports = router
+// get user profile
+router.get("/profile", profile);
+
+module.exports = router;
